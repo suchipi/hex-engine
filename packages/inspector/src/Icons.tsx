bb9dde708e905dfd17f4decabbb75c244fec3b3e
Lily Scott <me@suchipi.com>
2020-01-01 14:53:51 -0700

    Fix react svg property

--- packages/inspector/src/Icons.tsx
+++ packages/inspector/src/Icons.tsx
@@ -38,7 +38,7 @@ export function StepIcon() {
  - file, You can obtain one at http://mozilla.org/MPL/2.0/. */
   return (
     <svg viewBox="0 0 16 16" width="16px" height="16px">
-      <g fill-rule="evenodd">
+      <g fillRule="evenodd">
         <path
           fill="currentColor"
           d="M13.297 6.912C12.595 4.39 10.167 2.5 7.398 2.5A5.898 5.898 0 0 0 1.5 8.398a.5.5 0 0 0 1 0A4.898 4.898 0 0 1 7.398 3.5c2.75 0 5.102 2.236 5.102 4.898v.004L8.669 7.029a.5.5 0 0 0-.338.942l4.462 1.598a.5.5 0 0 0 .651-.34.506.506 0 0 0 .02-.043l2-5a.5.5 0 1 0-.928-.372l-1.24 3.098z"
