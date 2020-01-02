12f34488fb809843482247582f0efa39c623577c
Lily Scott <me@suchipi.com>
2020-01-01 18:49:04 -0700

    Size EditableString inputs to minimum possible that contains content, or 200px

--- packages/inspector/src/EditableString.tsx
+++ packages/inspector/src/EditableString.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 export default function EditableString({
   color,
@@ -13,26 +13,60 @@ export default function EditableString({
 }) {
   const [isEditing, setIsEditing] = useState(false);
   const [editedValue, setEditedValue] = useState(value);
+  const measureWidthRef = useRef<HTMLDivElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | HTMLTextAreaElement | null>(null);
 
   const TagName = expanded ? "textarea" : "input";
 
+  useEffect(() => {
+    if (expanded) return;
+
+    const measureWidth = measureWidthRef.current;
+    const input = inputRef.current;
+
+    if (!measureWidth) return;
+    if (!input) return;
+
+    measureWidth.style.display = "inline";
+    const rect = measureWidth.getBoundingClientRect();
+    measureWidth.style.display = "none";
+
+    input.style.width = rect.width + "px";
+  }, [editedValue, value]);
+
   return (
-    <TagName
-      style={{ color }}
-      value={isEditing ? editedValue : value}
-      onFocus={() => {
-        setEditedValue(value);
-        setIsEditing(true);
-      }}
-      onChange={(
-        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-      ) => {
-        setEditedValue(event.target.value);
-        onChange(event.target.value);
-      }}
-      onBlur={() => {
-        setIsEditing(false);
-      }}
-    />
+    <>
+      <span
+        style={{ display: "none" }}
+        ref={measureWidthRef}
+        className="measure-width"
+      >
+        {isEditing ? editedValue : value}
+      </span>
+      <TagName
+        // @ts-ignore
+        ref={inputRef}
+        style={{ color, font: "inherit", maxWidth: "200px" }}
+        value={isEditing ? editedValue : value}
+        onFocus={() => {
+          setEditedValue(value);
+          setIsEditing(true);
+        }}
+        onChange={(
+          event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+        ) => {
+          if (!expanded && measureWidthRef.current) {
+            event.target.style.width =
+              measureWidthRef.current.getBoundingClientRect().width + "px";
+          }
+
+          setEditedValue(event.target.value);
+          onChange(event.target.value);
+        }}
+        onBlur={() => {
+          setIsEditing(false);
+        }}
+      />
+    </>
   );
 }
