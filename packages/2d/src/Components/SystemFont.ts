fca33f0d98729e2d196fbb68a3639d67cb7f5812
Lily Scott <me@suchipi.com>
2020-01-05 00:38:58 -0700

    Add name/size/color/align properties to SystemFont so you can play with it in the inspector

--- packages/2d/src/Components/SystemFont.ts
+++ packages/2d/src/Components/SystemFont.ts
@@ -11,27 +11,66 @@ export default function SystemFont({
   size: string | number;
   color?: void | string;
   align?: void | "start" | "end" | "left" | "right" | "center";
-}): FontImplementation {
+}) {
   useType(SystemFont);
 
-  if (typeof size === "number") {
-    size = size + "px";
+  const state = {
+    name,
+    size,
+    color,
+    align,
+  };
+
+  function prepareContext(context: CanvasRenderingContext2D) {
+    context.font =
+      (typeof state.size === "number" ? `${state.size}px` : state.size) +
+      " " +
+      state.name;
+
+    context.fillStyle = state.color;
+    context.textAlign = state.align;
   }
 
-  const fontString = size + " " + name;
+  const api: FontImplementation = {
+    drawText({ context, text, x = 0, y = 0 }) {
+      prepareContext(context);
+      context.fillText(text, x, y);
+    },
+    measureTextWidth({ context, text }) {
+      prepareContext(context);
+      const metrics = context.measureText(text);
+      return metrics.width;
+    },
+  };
 
-  return useNewComponent(() =>
-    Font({
-      drawText({ context, text, x = 0, y = 0 }) {
-        context.font = fontString;
-        context.fillStyle = color;
-        context.textAlign = align;
-        context.fillText(text, x, y);
-      },
-      measureTextWidth({ context, text }) {
-        const metrics = context.measureText(text);
-        return metrics.width;
-      },
-    })
-  );
+  useNewComponent(() => Font(api));
+
+  return {
+    ...api,
+
+    get name() {
+      return state.name;
+    },
+    set name(nextValue) {
+      state.name = nextValue;
+    },
+    get size() {
+      return state.size;
+    },
+    set size(nextValue) {
+      state.size = nextValue;
+    },
+    get color() {
+      return state.color;
+    },
+    set color(nextValue) {
+      state.color = nextValue;
+    },
+    get align() {
+      return state.align;
+    },
+    set align(nextValue) {
+      state.align = nextValue;
+    },
+  };
 }
--- packages/game/src/index.tsx
+++ packages/game/src/index.tsx
@@ -3,159 +3,16 @@ import * as hex from "@hex-engine/2d";
 window.hex = hex;
 
 import {
-  Audio,
-  Aseprite,
-  Canvas,
-  Vec2,
-  Angle,
-  Keyboard,
-  Position,
-  Origin,
-  AnimationSheet,
-  Animation,
-  AnimationFrame,
-  BasicRenderer,
-  Size,
-  useUpdate,
-  useExistingComponentByType,
-  useNewComponent,
-  useDraw,
   createEntity,
   useEntityName,
-  Camera,
-  Rotation,
-  useType,
+  useNewComponent,
+  Canvas,
   Component,
-  TileMap,
-  SpriteSheet,
-  Grid,
+  Position,
+  Vec2,
+  SystemFont,
+  useDraw,
 } from "@hex-engine/2d";
-import bouncy from "./bouncy-29x41.png";
-import jump from "./jump.wav";
-import slimeBlue from "./slime-blue.aseprite";
-
-// @ts-ignore
-window.slimeBlue = slimeBlue;
-
-const camera = createEntity(() => {
-  useEntityName("camera");
-  useNewComponent(() => Position(new Vec2(-100, -100)));
-  useNewComponent(() => Rotation(new Angle(0)));
-  useNewComponent(Camera);
-});
-
-const player = createEntity(() => {
-  useEntityName("player");
-  useNewComponent(() => Keyboard());
-  useNewComponent(() => Position(new Vec2(0, 0)));
-  const size = useNewComponent(() => Size(new Vec2(29, 41)));
-  useNewComponent(() => Origin(size.dividedBy(2)));
-  useNewComponent(() => Rotation(0));
-
-  const jumpSound = useNewComponent(() => Audio({ url: jump }));
-  const animSheet = useNewComponent(() =>
-    AnimationSheet({
-      url: bouncy,
-      tileWidth: 29,
-      tileHeight: 41,
-      animations: {
-        default: useNewComponent(() =>
-          Animation(
-            [0, 1, 2, 3, 4, 5, 6, 7].map(
-              (num) =>
-                new AnimationFrame(num, {
-                  duration: 150,
-                  onFrame:
-                    num === 2
-                      ? () => {
-                          jumpSound.play({ volume: 0.0 });
-                        }
-                      : null,
-                })
-            )
-          )
-        ),
-      },
-    })
-  );
-  animSheet.currentAnim.play();
-
-  useNewComponent(BasicRenderer);
-  useNewComponent(function PlayerControls() {
-    useType(PlayerControls);
-
-    return useUpdate((delta) => {
-      const keyboard = useExistingComponentByType(Keyboard)!;
-      const vector = keyboard.vectorFromKeys("w", "s", "a", "d");
-      vector.magnitude *= delta * 0.1;
-
-      const position = useExistingComponentByType(Position)!;
-      position.replace(position.add(vector.toVec2()).round());
-    });
-  });
-});
-
-const slime = createEntity(() => {
-  const position = useNewComponent(() =>
-    Position(new Vec2(100, 50))
-  ).asWorldPosition();
-
-  const aseprite = useNewComponent(() => Aseprite(slimeBlue));
-  aseprite.currentAnim.play();
-
-  useDraw((context) => {
-    aseprite.drawCurrentFrameIntoContext({
-      context,
-      x: position.x,
-      y: position.y,
-    });
-  });
-});
-
-const stage = createEntity(() => {
-  useEntityName("stage");
-  const position = useNewComponent(() => Position(new Vec2(0, 0)));
-  const size = useNewComponent(() => Size(new Vec2(50, 50)));
-
-  return useDraw((context) => {
-    context.strokeStyle = "black";
-    context.strokeRect(
-      position.x - size.x / 2,
-      position.y - size.y / 2,
-      size.x,
-      size.y
-    );
-  });
-});
-
-const bg = createEntity(() => {
-  useEntityName("bg");
-
-  useNewComponent(Position);
-  useNewComponent(() => Rotation(0));
-
-  const sheet = useNewComponent(() =>
-    SpriteSheet({ tileWidth: 29, tileHeight: 41, url: bouncy })
-  );
-
-  const grid = new Grid(3, 3, 0);
-  // prettier-ignore
-  grid.setData([
-    0, 1, 2,
-    3, 4, 5,
-    6, 7, 0
-  ]);
-
-  const size = useNewComponent(() => Size(sheet.tileSize.times(3)));
-  useNewComponent(() => Origin(size.dividedBy(2)));
-
-  useNewComponent(() => TileMap(sheet, grid));
-
-  return {
-    ...useNewComponent(BasicRenderer),
-    grid,
-  };
-});
 
 const canvas = createEntity(() => {
   useEntityName("canvas");
@@ -176,10 +33,6 @@ const canvas = createEntity(() => {
 
       // Then draw non-cameras, sorted by entity world position y
       const sortedEnts = [...entities].sort((entA, entB) => {
-        // Always draw stage first
-        if (entA === stage) return -1;
-        if (entB === stage) return 1;
-
         const posA =
           entA.getComponent(Position)?.asWorldPosition() || new Vec2(0, 0);
         const posB =
@@ -195,11 +48,20 @@ const canvas = createEntity(() => {
   );
 });
 
-canvas.addChild(bg);
-canvas.addChild(stage);
-canvas.addChild(camera);
-stage.addChild(player);
-stage.addChild(slime);
+const label = createEntity(() => {
+  const font = useNewComponent(() => SystemFont({ name: "Arial", size: 24 }));
+
+  useDraw((context) => {
+    font.drawText({
+      context,
+      text: "Hello world",
+      x: 100,
+      y: 100,
+    });
+  });
+});
+
+canvas.addChild(label);
 
 // @ts-ignore
 window.canvas = canvas;
