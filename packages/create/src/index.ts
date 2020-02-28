f05004ff5b5e7d7be32eb8b790a9b32ce3dedccd
Emilien <admin@emilienleroy.fr>
2020-02-28 20:51:15 +0100

    Add ora dependency and add simple spinner in create

--- packages/create/src/index.ts
+++ packages/create/src/index.ts
@@ -1,6 +1,8 @@
 import path from "path";
+import ora from "ora";
 import { cp, cd, exec } from "shelljs";
 
+const spinner = ora();
 const templateDir = path.resolve(__dirname, "..", "template");
 
 export default function makeIt(targetDirectory: string) {
@@ -8,9 +10,14 @@ export default function makeIt(targetDirectory: string) {
 
   cp("-r", templateDir, targetDirectory);
   cd(targetDirectory);
-  exec("npm install --save @hex-engine/2d@latest");
-  exec("npm install --save-dev hex-engine-scripts@latest");
-  exec("npm install --save-dev typescript@latest");
+
+  spinner.start("Installing dependencies ...");
+
+  exec("npm install --save @hex-engine/2d@latest", { silent: true });
+  exec("npm install --save-dev hex-engine-scripts@latest", { silent: true });
+  exec("npm install --save-dev typescript@latest", { silent: true });
+
+  spinner.succeed('Dependencies installed !');
 
   console.log("");
   console.log(`All done! To start working on your game, run:
