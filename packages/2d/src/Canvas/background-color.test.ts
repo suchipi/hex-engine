ff4b93a07274374508cb07ea76bcc10f65f6c289
Lily Scott <me@suchipi.com>
2020-04-03 04:16:27 -0600

    Instead of trying to make font consistent, hide inspector

--- packages/2d/src/Canvas/background-color.test.ts
+++ packages/2d/src/Canvas/background-color.test.ts
@@ -1,11 +1,14 @@
 /// <reference types="@test-it/core/globals" />
 import { createRoot, useNewComponent } from "@hex-engine/core";
+import Inspector from "@hex-engine/inspector";
 import Canvas from "./index";
 
 it("canvas background color", async () => {
-  createRoot(() => {
-    useNewComponent(() => Canvas({ backgroundColor: "green" }));
+  const rootEnt = createRoot(() => {
+    useNewComponent(() => Canvas({ backgroundColor: "#006400" }));
   });
+  const inspector = rootEnt.getComponent(Inspector)!;
+  inspector.hide();
 
   expect(await TestIt.captureScreenshot()).toMatchImageSnapshot();
 });
