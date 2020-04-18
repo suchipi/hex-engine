598aad46e4fd4ac72e39ef463fc39c071e69bd74
Emilien <admin@emilienleroy.fr>
2020-04-18 22:19:54 +0200

    Read gif using gifken

--- packages/2d/src/Components/GIF.ts
+++ packages/2d/src/Components/GIF.ts
@@ -1,14 +1,45 @@
 import { AnimationAPI, AnimationFrame } from './Animation';
 import { useType } from '@hex-engine/core';
+import gifken from 'gifken';
 
-export default function GIF(): AnimationAPI<unknown> {
+interface GIFInterface extends AnimationAPI<{}> {
+    drawCurrentFrame(context: CanvasRenderingContext2D): void;
+}
+
+export default function GIF(options: { url: string }): GIFInterface {
     useType(GIF);
+    let frames: any[] = [];
+    let i = 0;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", options.url, true);
+    xhr.responseType = "arraybuffer";
+    xhr.onload = (e: any) => {
+        var arrayBuffer = e.target["response"];
+        var gif = gifken.Gif.parse(arrayBuffer);
+
+        frames = gif.split(false).map(frame => {
+            const url = URL.createObjectURL(new Blob(frame.writeToArrayBuffer())).toString();
+            const img = document.createElement('img');
+            img.src = url;
+            return img
+        })
+    };
+    xhr.send();
 
     return {
+        drawCurrentFrame(context: CanvasRenderingContext2D) {
+            if(frames.length !== 0) {
+                context.drawImage(frames[i], 0, 0);
+                if(frames.length - 1 > i) {
+                    i++;
+                }
+            }
+        },
         frames: [],
         loop: false, 
         currentFrameIndex: 0,
-        currentFrame: new AnimationFrame(null, {
+        currentFrame: new AnimationFrame({}, {
             duration: 0,
         }),
         currentFrameCompletion: 0,
@@ -23,11 +54,9 @@ export default function GIF(): AnimationAPI<unknown> {
         },
         restart() {
             return void 0;
-        },
-      
-        
+        },  
         goToFrame(frameNumber: number) {
-            return void 0;
+            return frameNumber;
         },
     }
 }
