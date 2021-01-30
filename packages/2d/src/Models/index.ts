ab20a9805571f5b3798c67930b8bd64b55052867
Lily Scott <me@suchipi.com>
2021-01-30 15:06:53 -0700

    export ReadOnlyVector

--- packages/2d/src/Models/index.ts
+++ packages/2d/src/Models/index.ts
@@ -1,9 +1,19 @@
 import Circle from "./Circle";
 import Grid from "./Grid";
-import Vector from "./Vector";
+import Vector, { ReadOnlyVector /* @babel-remove-prev-node */ } from "./Vector";
 import Polygon from "./Polygon";
 import TransformMatrix from "./TransformMatrix";
 
 type Shape = Circle | Polygon;
 
-export { Circle, Grid, Vector, Polygon, Shape, TransformMatrix };
+// NOTE: You have to put a babel-remove-prev-node pragma comment after each
+// type-only export, otherwise there ends up being issues with the build later.
+export {
+  Circle,
+  Grid,
+  Vector,
+  ReadOnlyVector /* @babel-remove-prev-node */,
+  Polygon,
+  Shape,
+  TransformMatrix,
+};
