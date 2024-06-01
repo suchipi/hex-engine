6378f59d34413b296417d3c92225f856a75c4604
Lily Skye <me@suchipi.com>
2024-06-01 17:08:49 -0600

    bump versions of everything in scripts package and update target browsers

--- packages/scripts/babel.config.js
+++ packages/scripts/babel.config.js
@@ -5,13 +5,10 @@ module.exports = {
       "@babel/preset-env",
       {
         modules: "commonjs",
+        // https://browsersl.ist/#q=defaults%0A
+        targets: "defaults",
       },
     ],
     "@babel/preset-react",
-  ],
-  plugins: [
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-    "@babel/plugin-proposal-optional-chaining",
   ],
 };
