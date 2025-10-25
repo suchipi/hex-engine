bb770776ad8e33cf6acb2fab19ec4b456d9991e6
Lily Skye <git@suchipi.tv>
2025-10-25 02:45:49 -0600

    wrangling package build

--- packages/website/in-docker.sh
+++ packages/website/in-docker.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env fish
 # local fish version at time of writing was 3.6.1
 
+set fish_trace 1
+
 cd (dirname (status --current-filename))
 
-set -l tty_arg ''
+set -l rm_and_tty_arg '--rm'
 if isatty 0 # stdin
-  set tty_arg '-it'
+  set rm_and_tty_arg '--rm -it'
 end
 
 set -l node_version (string replace 'v' '' (cat ../../.node-version))
 
 mkdir -p ./node_modules/.docker
 
-docker run --rm $tty_arg \
+docker run $rm_and_tty_arg \
   -p 3000:3000 \
   -v "$PWD":/app -w /app \
   -v "$PWD"/node_modules/.docker:/app/node_modules \
