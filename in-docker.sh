c188dcae4787e0d0513e942572371ae43ecb289e
Lily Skye <git@suchipi.tv>
2025-10-26 00:32:46 -0600

    fix in-docker

--- in-docker.sh
+++ in-docker.sh
@@ -2,4 +2,9 @@
 set -e
 
 mkdir -p node_modules/.docker
-docker run --rm -it -v "$PWD:/hex-engine" -v "$PWD/node_modules/.docker:/hex-engine/node_modules" suchipi/node-nw-env:0.7.0 bash -c "Xvfb -screen 0 1024x768x16 -ac & source /usr/local/nvm/nvm.sh && cd /app && nvm install && nvm use && $*"
+docker run --rm -it \
+  -v "$PWD:/hex-engine" \
+  -v "$PWD/node_modules/.docker:/hex-engine/node_modules" \
+    suchipi/node-nw-env:0.7.0 \
+      bash -c \
+        "Xvfb -screen 0 1024x768x16 -ac & source /usr/local/nvm/nvm.sh && cd /hex-engine && nvm install && nvm use && $*"
