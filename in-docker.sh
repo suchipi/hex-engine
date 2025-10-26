85959b08feb2fc3e40519be77bfd3ff8dda11876
Lily Skye <git@suchipi.tv>
2025-10-26 00:37:30 -0600

    oops, need the tty it handling stuff

--- in-docker.sh
+++ in-docker.sh
@@ -1,10 +1,19 @@
-#!/usr/bin/env bash
-set -e
+#!/usr/bin/env fish
+# local fish version at time of writing was 4.0.2
+
+set fish_trace 1
+
+cd (dirname (status --current-filename))
+
+set -l rm_and_tty_arg '--rm'
+if isatty 0 # stdin
+  set -a rm_and_tty_arg '-it'
+end
 
 mkdir -p node_modules/.docker
-docker run --rm -it \
+docker run $rm_and_tty_arg \
   -v "$PWD:/hex-engine" \
   -v "$PWD/node_modules/.docker:/hex-engine/node_modules" \
     suchipi/node-nw-env:0.7.0 \
       bash -c \
-        "Xvfb -screen 0 1024x768x16 -ac & source /usr/local/nvm/nvm.sh && cd /hex-engine && nvm install && nvm use && $*"
+        "Xvfb -screen 0 1024x768x16 -ac & source /usr/local/nvm/nvm.sh && cd /hex-engine && nvm install && nvm use && $argv"
