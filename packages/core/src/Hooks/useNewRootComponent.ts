0b5b84f230266d1669ffe74121f05f92a6269e6f
Lily Skye <git@suchipi.tv>
2026-04-07 18:43:38 -0600

    fix missed spot for ENTITY_ADD_COMPONENT event

--- packages/core/src/Hooks/useNewRootComponent.ts
+++ packages/core/src/Hooks/useNewRootComponent.ts
@@ -1,3 +1,8 @@
+import {
+  CoreEventPhase,
+  CoreEventType,
+  eventSystemSingleton,
+} from "../EventSystem";
 import instantiate from "../instantiate";
 import { Component } from "../Interface";
 import useRootEntity from "./useRootEntity";
@@ -8,7 +13,23 @@ export default function useNewRootComponent<T>(
   const rootEntity = useRootEntity();
 
   const child = instantiate(componentFunction, rootEntity);
+
+  eventSystemSingleton.emit({
+    eventType: CoreEventType.ENTITY_ADD_COMPONENT,
+    eventPhase: CoreEventPhase.BEFORE,
+    componentFactory: componentFunction,
+    entity: rootEntity,
+  });
+
   rootEntity.components.add(child);
+
+  eventSystemSingleton.emit({
+    eventType: CoreEventType.ENTITY_ADD_COMPONENT,
+    eventPhase: CoreEventPhase.AFTER,
+    componentFactory: componentFunction,
+    entity: rootEntity,
+    component: child,
+  });
 
   return child;
 }
